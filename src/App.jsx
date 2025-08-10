0db564a7a118ccccb87ec5cced01060568286fe8
Mansi <mansi.22420063@viit.ac.in>
2025-08-10 11:33:49 +0530

    Add EcoMart customer dashboard with components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Customer from './components/Customer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Customer />
+    </div>
+  )
+}
+
+export default App
